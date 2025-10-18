e9d4d61a01bbe46b7ddc4ce6aea11c35b8ead12f
SelamawitZeree <selamawitzere7@gmail.com>
2025-10-17 23:30:02 -0500

    feat: Initial project structure imported, Maven configured, and domain/service packages created.

---
+++ src/main/java/edu/miu/cs/cs489/pensions/cli/PensionCLIApp.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs489.pensions.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionsCliAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PensionsCliAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/cs/cs489/pensions/cli/PensionsCliAppApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs489.pensions.cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionsCliAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
